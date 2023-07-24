16147012617c95ec11bfd614643ff464a19a692b
Mateus Castro <mateuslopescastro@icloud.com>
2023-07-24 17:32:02 -0300

    feat(schedule): register a new scheduling

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,11 +1,13 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { useRouter } from 'next/router'
 import { CalendarBlank, Clock } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 import { Form, FormHeader, FormActions, FormError } from './styles'
 import dayjs from 'dayjs'
+import { api } from '../../../../../lib/axios'
 
 interface ConfirmStepProps {
   onCancelConfirmation: () => void
@@ -32,8 +34,20 @@ export function ConfirmStep({
     resolver: zodResolver(confirmFormSchema),
   })
 
+  const router = useRouter()
+  const username = String(router.query.username)
+
   async function onSubmit(data: ConfirmFormData) {
-    //
+    const { email, name, notes } = data
+
+    await api.post(`/users/${username}/schedule`, {
+      name,
+      email,
+      notes,
+      date: schedulingDate,
+    })
+
+    onCancelConfirmation()
   }
 
   const describedDate = dayjs(schedulingDate).format('DD[ de ]MMMM[ de ]YYYY')
